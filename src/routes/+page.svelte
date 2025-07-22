cba41cf4f3f83ad5694f45f26fad431127496e1d
robertbrice <rob.a.brice@gmail.com>
2025-07-22 21:46:11 +0100

    small change ot page text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,3 @@
 <h1>Welcome to SvelteKit</h1>
+<h1>This is test3 to check if an updated build actually works on aws amplify!!</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
